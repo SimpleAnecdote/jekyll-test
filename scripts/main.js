35a3bd90cb1d3f7ab9ec1f062d98c498602d1086
Jordie Knowles <jordie@thehaystackapp.com>
2016-09-26 15:55:03 +1000

    Header now has a "transparent" class when scroll position is at the top.

---
+++ scripts/main.js
@@ -0,0 +1,59 @@
+(function () {
+
+	var transparencyCutoff = 200;
+	var transparent = true;
+	var headerElement = document.getElementsByTagName('header')[0];
+	var transparentClassName = 'transparent';
+
+	// function hasClass(el, className) {
+	// 	if (el.classList) {
+	// 		return el.classList.contains(className);		
+	// 	} else {
+	// 	return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));		
+	// 	}
+	// }
+
+	function addClass(el, className) {
+		if (el.classList) {
+			el.classList.add(className);
+		} else if (!hasClass(el, className)) {
+			el.className += " " + className;
+		}
+	}
+
+	function removeClass(el, className) {
+		if (el.classList) {
+			el.classList.remove(className);
+		} else if (hasClass(el, className)) {
+			var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+			el.className=el.className.replace(reg, ' ');
+		}
+	}
+
+	function addHeaderTransparency() {
+		addClass(headerElement, transparentClassName);
+	}
+
+	function removeHeaderTransparency() {
+		removeClass(headerElement, transparentClassName);
+	}
+
+	function setHeaderTransparency() {
+		var distanceFromTop = window.pageYOffset;
+
+		if (distanceFromTop > transparencyCutoff && transparent === true) {
+			transparent = false;
+			removeHeaderTransparency();
+
+		} else if (distanceFromTop <= transparencyCutoff && transparent === false) {
+			transparent = true;
+			addHeaderTransparency();
+		}
+	}
+
+	addHeaderTransparency();
+	document.onscroll = function() {
+		setHeaderTransparency();
+	};
+
+})();
